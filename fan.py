87aee28bb58cbcf22aba4d63d72e9a9cec055da7
Datsudo <76833632+datsudo@users.noreply.github.com>
2024-06-15 20:23:01 +0800

    up

--- fan.py
+++ fan.py
@@ -12,9 +12,9 @@ def change_temp(
         curr_temp: float,
         error: float,
         error_dot: float
-) -> float:
-    c = random.randint(-5, -1)
-    h = random.randint(1, 5)
+) -> tuple[float, str]:
+    c = random.uniform(-5.0, -1.0)
+    h = random.uniform(1.0, 5.0)
 
     if error_dot < 0:
         if error < 0: return curr_temp + c, "C"
@@ -37,7 +37,8 @@ def main() -> None:
     error_curr = CMD-INIT_TEMP
     error_prev = CMD-INIT_TEMP
     for t in range(1, 101):
-        new_temp, _ = change_temp(curr_temp, error_curr, error_prev)
+        error_dot = error_prev - error_curr
+        new_temp, _ = change_temp(curr_temp, error_curr, error_dot)
 
         curr_temp = new_temp
         temp_hist.append(new_temp)
@@ -48,13 +49,14 @@ def main() -> None:
 
         time_hist.append(t)
 
+        curr_temp += random.uniform(-0.3, 0.3)
+
     plt.title("Temperature vs Time Graph")
     plt.xlabel("Time (s)"); plt.ylabel("Temperature (F)")
     plt.plot(time_hist, temp_hist, marker=".")
     plt.show()
 
     plt.title("Error vs Time Graph")
-    plt.ylim(-5, 5)
     plt.xlabel("Time (s)"); plt.ylabel("Error (cmd - temp)")
     plt.plot(time_hist, error_hist, marker=".")
     plt.show()
@@ -62,5 +64,4 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
 
